16bfcb1616d6254b789725a845f2bc08929ca0ad
Vix-kit-git <kvrsocialmedia2004@gmail.com>
2025-08-20 12:44:31 +0530

    Added some hands on of week 8

--- week7/ticketbookingapp/src/App.js
+++ week7/ticketbookingapp/src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+
+function LoginButton(props) {
+  return(
+    <button onClick={props.onClick} >
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return(
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if(isLoggedIn) {
+    return <UserGreeting/>
+  } else {
+    return <GuestGreeting/>
+  }
+}
+
+function UserGreeting() {
+  return(
+    <div>
+      <h1><strong>Welcome back</strong></h1>
+        
+    </div>
+    
+  );
+}
+
+function GuestGreeting() {
+  return(
+    <div>
+      <h1><strong>Please sign up.</strong></h1>
+      
+    </div>
+    
+  );
+  }
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const loginClick = () => setIsLoggedIn(true);
+  const logoutClick = () => setIsLoggedIn(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greeting isLoggedIn={isLoggedIn}/>
+      {
+        isLoggedIn ? (
+          <LogoutButton onClick={logoutClick}/>
+        ) : (
+          <LoginButton onClick={loginClick}/>
+        )
+      }
     </div>
   );
 }
